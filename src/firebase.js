e03b50ed65354ca2d5a9d0ab48c4eec08291fcf8
Vatsal <vatsalpatel1073@gmail.com>
2020-10-16 13:16:55 +0530

    Init

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB4aDrHxqDpD94uCJr0vnHsrhqkSNKVktU",
+  authDomain: "fir-33ce1.firebaseapp.com",
+  databaseURL: "https://fir-33ce1.firebaseio.com",
+  projectId: "fir-33ce1",
+  storageBucket: "fir-33ce1.appspot.com",
+  messagingSenderId: "727843532377",
+  appId: "1:727843532377:web:ca6ab2bd92cec49cb28b48",
+  measurementId: "G-H9D79N8JVN"
+}
